a25105f88478010256b2d88cb4091ee8e7053f5f
Jonatas Sampaio <aurijona192@gmail.com>
2026-06-10 21:40:31 -0300

    Ajustando projeto

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 
+        String mensagem = "Oi, comprei uma calça jeans tamanho 44 ontem.";
+
+        mensagem = normalizar(mensagem);
+
         List<Palavra> palavras = Palavras.getPalavras();
 
         int setorVendas = 0;
@@ -11,16 +15,24 @@ public class Main {
         int setorFinanceiro = 0;
 
         for (Palavra palavra : palavras) {
-            if(palavra.getSetor().equals(Setor.VENDAS)){
-                setorVendas++;
-            }else if(palavra.getSetor().equals(Setor.FINANCEIRO)){
-                setorFinanceiro++;
+            if (mensagem.contains(palavra.getPalavra())) {
+                if (palavra.getSetor().equals(Setor.VENDAS)) {
+                    setorVendas++;
+                } else if (palavra.getSetor().equals(Setor.FINANCEIRO)) {
+                    setorFinanceiro++;
+                }
             }
         }
 
-        if(setorFinanceiro > setorVendas){
-            System.out.println("Redirecionando ao setor de vendas");
+        if (setorFinanceiro > setorVendas) {
+            System.out.println("Redirecionando ao setor de financeiro");
         }
 
+    }
+
+    private static String normalizar(String text) {
+        return text.toLowerCase()
+                .replaceAll("[^a-záéíóúãõâêô\\s]", "")
+                .trim();
     }
 }
--- src/Palavras.java
+++ src/Palavras.java
@@ -5,7 +5,7 @@ public class Palavras {
 
     public static List<Palavra> listaDePalavras =  List.of(
             new Palavra("valor",2, Setor.FINANCEIRO),
-            new Palavra("cobrado",2,Setor.FINANCEIRO),
+            new Palavra("comprei",2,Setor.FINANCEIRO),
             new Palavra("cartão",1,Setor.FINANCEIRO),
             new Palavra("calça",1,Setor.VENDAS),
             new Palavra("tamanho 44",1,Setor.VENDAS)
